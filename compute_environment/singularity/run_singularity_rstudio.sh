a48622d3fc4cbf43ef18cbf39237cdef2fc36e28
Josh Granek <josh@duke.edu>
2019-08-21 23:26:55 -0400

    run_singularity_rstudio.sh: update to use image from sylabs and use "private" /tmp

--- compute_environment/singularity/run_singularity_rstudio.sh
+++ compute_environment/singularity/run_singularity_rstudio.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
 set -u
-# SINGULARITY_IMAGE="singularity run shub://granek/crne_transposon:rstudio --app rstudio"
 SINGULARITY_DIR="$HOME/container_images"
 SINGULARITY_CACHEDIR="$SINGULARITY_DIR/cachedir"
 export SINGULARITY_CACHEDIR
 SINGULARITY_PULLFOLDER="$SINGULARITY_DIR/pulldir"
 export SINGULARITY_PULLFOLDER
 
-SESSION_INFO_DIR="$SINGULARITY_DIR/session_info"
-mkdir -p $SESSION_INFO_DIR
+SINGULARITY_IMAGE="${1:-library://granek/default/mar1_rnaseq:latest}"
 
-# SINGULARITY_IMAGE="shub://granek/mar1_rnaseq:rstudio"
-SINGULARITY_IMAGE="${1:-$SINGULARITY_DIR/mar1_rstudio.simg}"
-
-# DATA_BASE_DIR="${DATA_BASE_DIR:-$HOME}"
-# WORKSPACE_BASE_DIR="${WORKSPACE_BASE_DIR:-$HOME}"
+DATA_BASE_DIR="${DATA_BASE_DIR:-$HOME}"
+WORKSPACE_BASE_DIR="${WORKSPACE_BASE_DIR:-$HOME}"
 
 DATA="$DATA_BASE_DIR/hts2018/rawdata"
 WORKSPACE="$WORKSPACE_BASE_DIR/hts2018/workspace"
+
+ 
 
  
 if [ -d "${DATA}" ]; then
@@ -34,32 +31,19 @@ else
     echo "Make sure WORKSPACE exists: $WORKSPACE"
 fi
 
+#-----------------
+SINGTMP="/tmp/${USER}_`date +%s`_tmp"
+mkdir -p $SINGTMP
+echo "Binding /tmp to $SINGTMP"
+
+trap "{ rm -rf $SINGTMP; }" EXIT
+
+BIND_ARGS="$BIND_ARGS --bind $SINGTMP:/tmp"
+#-----------------
+
 mkdir -p $SINGULARITY_PULLFOLDER $SINGULARITY_CACHEDIR
 #--------------------------------------------------------------------------------
 
-# Look for an open port to use in range $LOWERPORT to $UPPERPORT
-LOWERPORT=8780
-UPPERPORT=8799
-while :
-do
-        RSTUDIO_PORT="`shuf -i $LOWERPORT-$UPPERPORT -n 1`"
-        echo "Testing port: $RSTUDIO_PORT"
-        ss -lpn | grep -q ":$RSTUDIO_PORT " || break
-done
-
-SESSION_INFO_FILE="$SESSION_INFO_DIR/session_info_$(basename $SINGULARITY_IMAGE)_${RSTUDIO_PORT}.txt"
-echo $SESSION_INFO_FILE
-export RSTUDIO_PASSWORD="`openssl rand -base64 16 | colrm 20`"
-
-# Make $SESSION_INFO_FILE only readable by owner
-rm -f $SESSION_INFO_FILE; touch $SESSION_INFO_FILE; chmod 600 $SESSION_INFO_FILE
-printf "\n\nRStudio URL:\t\thttp://`hostname -A | cut -f1 -d' '`:${RSTUDIO_PORT}/\n" >> $SESSION_INFO_FILE
-printf "\nRStudio Username:\t$USER\n"  >> $SESSION_INFO_FILE
-printf "RStudio Password:\t$RSTUDIO_PASSWORD\n" >> $SESSION_INFO_FILE
-
-cat $SESSION_INFO_FILE
-trap "{ rm -f $SESSION_INFO_FILE; }" EXIT
-
-singularity run  --app rstudio $BIND_ARGS $SINGULARITY_IMAGE --auth-none 0 --auth-pam-helper rstudio_auth --www-port $RSTUDIO_PORT
+singularity run $BIND_ARGS $SINGULARITY_IMAGE
 
 
