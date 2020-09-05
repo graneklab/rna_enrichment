f9b53f899be7a23b4cf2d2a31f3647bd56db6b73
Josh Granek <josh@duke.edu>
2020-09-04 20:04:12 -0400

    Working on analysis combined 2018 and 2019 data

---
+++ combined_analysis/combined_config.R
@@ -0,0 +1,5 @@
+
+starout_2019_dir="/workspace/2019_enrich_samples/enrich_compare_starout"
+starout_2018_dir="/workspace/2018_tot_samples/enrich_compare_starout"
+count_suffix = "_ReadsPerGene.out.tab"
+
---
+++ combined_analysis/shared_functions.R
@@ -0,0 +1,41 @@
+mycombine <- function(df1, df2) {
+  # Combine two data frames by gene names
+  #
+  # Args:
+  #   df1 (Dataframe): the first count data
+  #   df2 (Dataframe): the second count data
+  #
+  # Returns:
+  #   (Dataframe) The combined data frame of df1 and df2
+  full_join(df1, df2, by = "gene")
+}
+
+loadStarCounts = function(countfile_paths) {
+  # Data type for each column
+  coltypes <-
+    list(col_character(),
+         col_integer(),
+         col_integer(),
+         col_integer())
+  
+  out <- foreach(count_path = countfile_paths, .combine = mycombine) %do% {
+    # generate rowname (extracted from count file name)
+    count_path %>%
+      path_file %>%
+      str_replace(count_suffix, "") ->
+      readset_label
+    
+    # read in the count file
+    readr::read_tsv(count_path, col_names = FALSE, col_types = coltypes) %>%
+      dplyr::select(X1, X4) %>% # get the 1st and 4th columns (gene ids and second strand read counts)
+      dplyr::rename_(.dots = setNames(names(.), c("gene", readset_label)))
+  }
+  
+  
+  # Drop STAR count statistics and rotate dataframe
+  out %>%
+    dplyr::slice(-(1:4)) %>%
+    gather(expid, value,-gene) %>%
+    spread(gene, value) -> genecounts
+  return(genecounts)
+}
