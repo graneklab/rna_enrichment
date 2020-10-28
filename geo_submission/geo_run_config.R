2ecdf584998292d073bde359c498f908da4f5936
Josh Granek <josh@duke.edu>
2020-10-27 21:49:22 -0400

    geo_submission.Rmd: switch to use "geo" versions of FASTQs and counts Use FASTQs downloaded using download_data.Rmd and counts generated using run_star_for_geo.Rmd

--- geo_submission/geo_run_config.R
+++ geo_submission/geo_run_config.R
@@ -2,14 +2,18 @@
 source("../common_config.R")
 
 geo_base_dir="/workspace/geo_dir"
+
 geo_upload_dir=file.path(geo_base_dir, "upload_to_geo")
 
 geo_fastq_dir=file.path(geo_upload_dir, "raw_fastqs")
 dir.create(geo_fastq_dir, recursive = TRUE)
 
-pilot2019_fastq_dir = file.path(geo_fastq_dir, "hts_2019_data/hts2019_pilot_rawdata")
-Sys.setenv(FASTQ_2018_DIR = file.path(geo_fastq_dir, "hts_2018_data/hts2018_pilot_rawdata"))
+pilot2018_fastq_dir = file.path(geo_fastq_dir, "hts_2018_data", "hts2018_pilot_rawdata")
+pilot2019_fastq_dir = file.path(geo_fastq_dir, "hts_2019_data", "hts2019_pilot_rawdata")
+
+Sys.setenv(FASTQ_2018_DIR = pilot2018_fastq_dir)
 Sys.setenv(FASTQ_2019_DIR = pilot2019_fastq_dir)
+
                       
 # output
 info.dir=file.path(geo_base_dir, "info")
@@ -20,6 +24,14 @@ Sys.setenv(GENOME_DIR = genome.dir)
 
 geo_starout_dir=file.path(geo_upload_dir, "geo_starout")
 Sys.setenv(GEO_STAROUT_DIR = geo_starout_dir)
+
+pilot2019_count_dir = geo_starout_dir
+pilot2018_count_dir = geo_starout_dir
+
+
+table_dir=file.path(geo_base_dir, "table_dir")
+dir.create(table_dir)
+sample_table_tsv=file.path(table_dir, "sample_table.tsv")
 
 
 # genome
