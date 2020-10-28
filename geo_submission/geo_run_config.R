705f003506d1602813f034cce0f803e7f77f632a
Josh Granek <josh@duke.edu>
2020-10-27 21:29:22 -0400

    geo_submission.Rmd: make GEO Samples table for 2019 libraries

--- geo_submission/geo_run_config.R
+++ geo_submission/geo_run_config.R
@@ -7,8 +7,9 @@ geo_upload_dir=file.path(geo_base_dir, "upload_to_geo")
 geo_fastq_dir=file.path(geo_upload_dir, "raw_fastqs")
 dir.create(geo_fastq_dir, recursive = TRUE)
 
+pilot2019_fastq_dir = file.path(geo_fastq_dir, "hts_2019_data/hts2019_pilot_rawdata")
 Sys.setenv(FASTQ_2018_DIR = file.path(geo_fastq_dir, "hts_2018_data/hts2018_pilot_rawdata"))
-Sys.setenv(FASTQ_2019_DIR = file.path(geo_fastq_dir, "hts_2019_data/hts2019_pilot_rawdata"))
+Sys.setenv(FASTQ_2019_DIR = pilot2019_fastq_dir)
                       
 # output
 info.dir=file.path(geo_base_dir, "info")
